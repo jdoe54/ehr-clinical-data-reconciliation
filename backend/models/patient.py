f9d22ddbbe29e15ee2ade5fd4d3c48848fa6f5d5
jdoe54 <148502754+jdoe54@users.noreply.github.com>
2026-05-05 18:16:27 -0700

    Added Postgres SQL database and Alemic migration

---
+++ backend/models/patient.py
@@ -0,0 +1,12 @@
+from datetime import date
+from sqlalchemy import String, Date, Integer
+from sqlalchemy.orm import Mapped, mapped_column
+from database import Base
+
+class Patient(Base):
+    __tablename__ = "patients"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    first_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    date_of_birth: Mapped[date | None] = mapped_column(Date, nullable=True)
